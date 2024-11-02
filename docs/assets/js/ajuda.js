047d513663eac650f425e0d013838f3633224360
Victor Wilson Medeiros da Silva <victorwilson2016@gmail.com>
2024-11-02 14:33:25 -0300

    Criação da página de Ajuda!
    Criação da página ajuda(a principal) e as páginas para cada tópico de ajuda.
    
    Além das páginas de JS e de CSS para controlar o comportamento e estilizar as páginas

---
+++ docs/assets/js/ajuda.js
@@ -0,0 +1,11 @@
+var botoes = document.querySelectorAll(".botaoAjuda");
+
+var contador = 0;
+console.log(botoes.length);
+
+botoes.forEach(function(){
+    
+    contador++;
+    console.log(contador);
+});
+
